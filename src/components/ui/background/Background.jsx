ee608c9a86f9e09a6d2a689692fc90efe110fcdf
Андрей Ильин <a.ilin.v@yandex.ru>
2023-04-20 17:14:58 +0300

    upd::background

--- src/components/ui/background/Background.jsx
+++ src/components/ui/background/Background.jsx
@@ -6,13 +6,10 @@ import { loadFull } from "tsparticles";
 
 const Background = () => {
     const particlesInit = useCallback(async (engine) => {
-        console.log(engine);
         await loadFull(engine);
     }, []);
 
-    const particlesLoaded = useCallback(async (container) => {
-        await console.log(container);
-    }, []);
+    const particlesLoaded = useCallback(async () => {}, []);
 
     return (
         <div className={styles.background}>
