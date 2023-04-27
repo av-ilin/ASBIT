db120cae8143a1be21816718f2df8425a123e1f0
Андрей Ильин <a.ilin.v@yandex.ru>
2023-04-27 11:03:35 +0300

    upd::nav

--- src/components/navigation/nav/Nav.jsx
+++ src/components/navigation/nav/Nav.jsx
@@ -1,13 +1,27 @@
 import styles from "./Nav.module.scss";
 
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 import Contacts from "./nav-contacts/Contacts";
 import Menu from "./nav-menu/Menu";
 
 const Navigation = () => {
+    const [isNavScroll, setIsNavScroll] = useState(false);
+
+    useEffect(() => {
+        const onScroll = () => {
+            console.log("scroll");
+        };
+        window.addEventListener("scroll", onScroll);
+        return () => {
+            window.removeEventListener("scroll", onScroll);
+        };
+    }, []);
+
     return (
         <div className={styles.nav}>
+            {isNavScroll && <div className={styles.bg}></div>}
             <div className={styles.container}>
                 <Link className={styles["logo-wrap"]} to="/">
                     <img
