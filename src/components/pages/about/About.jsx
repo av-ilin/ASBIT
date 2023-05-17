5843bfe985491653479d8fb2841e1ca51429fabc
Андрей Ильин <a.ilin.v@yandex.ru>
2023-05-17 17:49:04 +0300

    upd::about

--- src/components/pages/about/About.jsx
+++ src/components/pages/about/About.jsx
@@ -1,11 +1,7 @@
 import styles from "./About.module.scss";
 
 const About = () => {
-    return (
-        <div className={styles.about}>
-            <Activity />
-        </div>
-    );
+    return <div className={styles.about}></div>;
 };
 
 export default About;
