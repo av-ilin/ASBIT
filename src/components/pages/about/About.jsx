75caeaa372da539a694b7bf8c4ddbb8cc286fb8f
Андрей Ильин <a.ilin.v@yandex.ru>
2023-06-18 18:59:40 +0300

    upd::about

--- src/components/pages/about/About.jsx
+++ src/components/pages/about/About.jsx
@@ -1,12 +1,14 @@
 import styles from "./About.module.scss";
 
 import AboutAdvantages from "./about-advantages/AboutAdvantages";
+import AboutProfit from "./about-profit/AboutProfit";
 
 const About = () => {
     return (
         <div className={styles.about}>
             <div className="container">
                 <AboutAdvantages />
+                <AboutProfit />
             </div>
         </div>
     );
